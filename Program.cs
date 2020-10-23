d3d3dbe52e0ad182728fe098d941a0928092ea93
Elcio <elcio.ishizuka@gmail.com>
2020-10-23 19:47:36 -0300

    Aprendizado inicial em .NET - Hello World e variaveis

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Intro.NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int numero = 15;
+            char caracter = 'A';
+            string nome = "Elcio";
+            int idade = 40;
+            float preco = 10.59f;
+
+            Console.WriteLine(numero);
+            Console.WriteLine(caracter);
+            Console.WriteLine(nome);
+            Console.WriteLine(preco);
+
+            Console.WriteLine($"{nome} tem {idade} anos");
+
+
+
+        }
+
+
+    }
+}
