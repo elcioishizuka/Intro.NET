41bb300c121b9f8c6f4e8aa2c76b767e7f4b656a
Elcio <elcio.ishizuka@gmail.com>
2020-10-26 16:53:54 -0300

    Casting, conversao de dados e try..catch..

--- Program.cs
+++ Program.cs
@@ -21,10 +21,27 @@ namespace Intro.NET
 
             Console.WriteLine($"{nome} tem {idade} anos");
 
+            Console.WriteLine("\nTreinando Casting");
+            int i = 1000;
+            // Casting - Conversão explícita
+            // Se o i for maior que 255 então haverá perda de valor
+            // se converter para byte porque o range do byte é entre 0 e 255
+            byte byte1 = (byte) i;
+            Console.WriteLine(byte1);
 
-
+            Console.WriteLine("\nTreinando conversao de dados e try..catch..");
+            
+            try
+            {
+                Console.Write("Digite um número byte a ser convertido: ");
+                string numero2 = Console.ReadLine();
+                byte byte2 = Convert.ToByte(numero2);
+                Console.WriteLine($"O número convertido é {byte2}");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine($"O número tem que estar entre {byte.MinValue} e {byte.MaxValue}");
+            }
         }
-
-
     }
 }
