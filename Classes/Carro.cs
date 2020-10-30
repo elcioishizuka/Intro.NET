919e5f7c26353e893d226c60e039afb7557ccd68
Elcio <elcio.ishizuka@gmail.com>
2020-10-30 15:31:03 -0300

    Aprendizado em métodos especiais getter setter e construtores

--- Classes/Carro.cs
+++ Classes/Carro.cs
@@ -10,7 +10,78 @@ namespace Classes
         public double peso;
         public int anoFabricacao;
         public bool chaveNoContato;
+
+        // Metodo Construtor
+        public Carro(string marca, string modelo, string cor, double peso, int anoFabricacao, bool chaveNoContato)
+        {
+            this.setMarca(marca);
+            this.setModel(modelo);
+            this.setCor(cor);
+            this.setPeso(peso);
+            this.setAnoFabricacao(anoFabricacao);
+            this.setChaveNoContato(chaveNoContato);
+        }
+
+        // Utilizando mais de um construtor neste código
+        public Carro() 
+        {
+
+        }
+
         
+        // Metodo Getter
+        public string getMarca()
+        {
+            return(this.marca);
+        }
+        public string getModelo()
+        {
+            return(this.modelo);
+        }
+        public string getCor()
+        {
+            return(this.cor);
+        }
+        public double getPeso()
+        {
+            return(this.peso);
+        }
+        public int getAnoFabricacao()
+        {
+            return(this.anoFabricacao);
+        }
+        public bool getChaveNoContato()
+        {
+            return(this.chaveNoContato);
+        }
+
+
+        // Metodo Setter
+        public void setMarca(string　marca)
+        {
+            this.marca = marca;
+        }
+        public void setModel(string modelo)
+        {
+            this.modelo = modelo;
+        }
+        public void setCor(string cor)
+        {
+            this.cor = cor;
+        }
+        public void setPeso(double peso)
+        {
+            this.peso = peso;
+        }
+        public void setAnoFabricacao(int anoFabricacao)
+        {
+            this.anoFabricacao = anoFabricacao;
+        }
+        public void setChaveNoContato(bool chaveNoContato)
+        {
+            this.chaveNoContato = chaveNoContato;
+        }
+
         public void mostrarDados()
         {
             System.Console.WriteLine($"\nMarca: {this.marca}");
@@ -25,11 +96,11 @@ namespace Classes
         {
             if (this.chaveNoContato == true)
             {
-                Console.WriteLine("Motor foi partido com sucesso!");
+                Console.WriteLine("Motor foi partido com sucesso!\n");
             }
             else
             {
-                Console.WriteLine("Coloque a chave no contato!!");
+                Console.WriteLine("Coloque a chave no contato!!\n");
             }            
         }
 
--- Program.cs
+++ Program.cs
@@ -7,45 +7,66 @@ namespace Intro.NET
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Console.WriteLine("Hello World!");
 
-            int numero = 15;
-            char caracter = 'A';
-            string nome = "Elcio";
-            int idade = 40;
-            float preco = 10.59f;
+            // int numero = 15;
+            // char caracter = 'A';
+            // string nome = "Elcio";
+            // int idade = 40;
+            // float preco = 10.59f;
 
-            Console.WriteLine(numero);
-            Console.WriteLine(caracter);
-            Console.WriteLine(nome);
-            Console.WriteLine(preco);
+            // Console.WriteLine(numero);
+            // Console.WriteLine(caracter);
+            // Console.WriteLine(nome);
+            // Console.WriteLine(preco);
 
-            Console.WriteLine($"{nome} tem {idade} anos");
+            // Console.WriteLine($"{nome} tem {idade} anos");
 
-            Console.WriteLine("\nTreinando Casting");
-            int i = 1000;
-            // Casting - Conversão explícita
-            // Se o i for maior que 255 então haverá perda de valor
-            // se converter para byte porque o range do byte é entre 0 e 255
-            byte byte1 = (byte) i;
-            Console.WriteLine(byte1);
+            // Console.WriteLine("\nTreinando Casting");
+            // int i = 1000;
+            // // Casting - Conversão explícita
+            // // Se o i for maior que 255 então haverá perda de valor
+            // // se converter para byte porque o range do byte é entre 0 e 255
+            // byte byte1 = (byte) i;
+            // Console.WriteLine(byte1);
 
-            Console.WriteLine("\nTreinando conversao de dados e try..catch..");
+            // Console.WriteLine("\nTreinando conversao de dados e try..catch..");
             
-            try
-            {
-                Console.Write("Digite um número byte a ser convertido: ");
-                string numero2 = Console.ReadLine();
-                byte byte2 = Convert.ToByte(numero2);
-                Console.WriteLine($"O número convertido é {byte2}");
-            }
-            catch (OverflowException)
-            {
-                Console.WriteLine($"O número tem que estar entre {byte.MinValue} e {byte.MaxValue}");
-            }
-
+            // try
+            // {
+            //     Console.Write("Digite um número byte a ser convertido: ");
+            //     string numero2 = Console.ReadLine();
+            //     byte byte2 = Convert.ToByte(numero2);
+            //     Console.WriteLine($"O número convertido é {byte2}");
+            // }
+            // catch (OverflowException)
+            // {
+            //     Console.WriteLine($"O número tem que estar entre {byte.MinValue} e {byte.MaxValue}");
+            // }
             
-            // Aprendizado em Programação Orientada a Objeto - Classes e Objetos
+            // // Aprendizado em Programação Orientada a Objeto - Classes e Objetos
+            // Carro carro1 = new Carro();
+            // carro1.marca = "Ford";
+            // carro1.modelo = "Ka";
+            // carro1.anoFabricacao = 2001;
+            // carro1.cor = "Preto";
+            // carro1.peso = 1501.25;
+            // carro1.chaveNoContato = true;
+            // carro1.mostrarDados();
+            // carro1.partirMotor();
+            
+            // Carro carro2 = new Carro();
+            // carro2.marca = "Honda";
+            // carro2.modelo = "Fit";
+            // carro2.anoFabricacao = 2015;
+            // carro2.cor = "Prata";
+            // carro2.peso = 1890.36;
+            // carro2.chaveNoContato = false;
+            // carro2.mostrarDados();
+            // carro2.partirMotor();          
+
+            // Aprendizado em Programação Orientada a Objeto - Metodos Especiais
+            // Instanciando carro1 com metodo constutor2
             Carro carro1 = new Carro();
             carro1.marca = "Ford";
             carro1.modelo = "Ka";
@@ -54,19 +75,13 @@ namespace Intro.NET
             carro1.peso = 1501.25;
             carro1.chaveNoContato = true;
             carro1.mostrarDados();
-            carro1.partirMotor();
+            carro1.partirMotor();            
 
-            Carro carro2 = new Carro();
-            carro2.marca = "Honda";
-            carro2.modelo = "Fit";
-            carro2.anoFabricacao = 2015;
-            carro2.cor = "Prata";
-            carro2.peso = 1890.36;
-            carro2.chaveNoContato = false;
+            // Instanciando carro2 com metodo constutor1
+            Carro carro2 = new Carro("Honda", "Fit", "Prata", 1890.36, 2020, false);
+            System.Console.WriteLine("A marca do carro é: " + carro2.getMarca());
             carro2.mostrarDados();
-            carro2.partirMotor();          
-
-
+            carro2.partirMotor();
         }
     }
 }
